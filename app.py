c545885dc741dfbad2b523e3fbac8cb509adde8e
devlai23 <57195223+devlai23@users.noreply.github.com>
2024-01-11 17:27:29 -0800

    base website

--- app.py
+++ app.py
@@ -3,8 +3,16 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def index():
+def home():
     return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/scanner')
+def scanner():
+    return render_template('scanner.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
